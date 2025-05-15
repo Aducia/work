66d2299a5769a21dbda98b1b873ec55afcfec719
Aducia <makadavladislava@gmail.com>
2025-05-15 20:04:28 +0000

    work in proces

--- classwork/08.05.25/main.cpp
+++ classwork/08.05.25/main.cpp
@@ -3,28 +3,79 @@
  *               50
  *             /   \
  *            20   71
- *          /  \    |
+ *          /  \    \
  *          15  26  72
  *              /\
   *            25 30
  *
  * */
 #include <iostream>
-
+#include <stack>
+#include <queue>
 struct tree_node 
 {
        	int value;
 	tree_node() {
 		right = left = 0;
 	}	
+	//int x(int a){ 
+		//left++;
+	//return 0;	
+//	}
 	tree_node* left;
 	tree_node* right;
 };
+	void take_left(tree_node* start ){
+		tree_node* current_element = start;
+		while ( current_element != 0 ){
+			std::cout << current_element->value << std::endl;
+			current_element = current_element->left;
+		}
+	}
+
+	void take_right(tree_node* start){
+		tree_node* current_element = start;
+		while ( current_element != 0 ){
+			std::cout << current_element->value << std::endl;
+			current_element = current_element->right;
+		}
+	}
+	void DFS (tree_node* start){
+		tree_node* current_element = start;	
+		if( 0 == current_element )
+			return ;
+		std::cout << current_element->value << std::endl;
+		DFS ( current_element->left );
+		DFS ( current_element->right );
+		}
+	void BFS (tree_node* start){
+		tree_node* current_element = start;
+		if ( 0 == current_element )
+			return ;
+		std::queue < tree_node*> queue;
+		queue.push( current_element );
+	while( 0 == queue.empty()) {
+		tree_node* current_element = queue.front();
+		queue.pop();
+		std::cout << current_element->value << std::endl;
+		if ( current_element->left !=0  ){
+			queue.push( current_element->left );}
+		if ( current_element->right !=0  ){
+			 queue.push( current_element->right );}
+	}}
+
+
+// int tree_node__x(tree_node* this,int a){
+// 	this->left++;
+// 	return 0;
+// }
 
 
 int main(){
 
 	tree_node root;
+	// tree_node__x(&root, 4);
+	//root.x(4);
 	root.value = 50;
 	tree_node l1_1;
 	l1_1.value = 20;
@@ -44,4 +95,13 @@ int main(){
 	l1_2.right = &l2_3;
 	l2_2.left = &l3_1;
 	l2_2.right = &l3_2;
+	take_left( &root );
+	std::cout << "********"<< std::endl;
+	take_right( &l1_1 );
+	std::cout << "********"<< std::endl;
+	DFS( &root );
+	std::cout << "********"<< std::endl;
+        BFS( &root );
+
+	return 0;
 }
